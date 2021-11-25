2363c0dd97705a35166f711d544a84bcae866b2b
alpharmike95 <55023198+alpharmike95@users.noreply.github.com>
2021-11-25 10:28:07 +0100

    Update train.py

--- train.py
+++ train.py
@@ -65,7 +65,7 @@ def main():
             args.start_epoch = 0
     else:
         model = models.MobileNet(decoder='upconv', output_size=(224,224), in_channels=3, pretrained=True)
-    # output_directory = os.path.dirname(args.resume)
+    
             
     # define loss function (criterion) and optimizer
     if args.criterion == 'l2':
@@ -73,7 +73,49 @@ def main():
     elif args.criterion == 'l1':
         criterion = criteria.MaskedL1Loss().cuda()        
     
-    train(train_loader, model, args.start_epoch, write_to_file=False)
+    # create results folder, if not already exists
+    output_directory = os.path.dirname('./checkpoint')
+    # output_directory = utils.get_output_directory(args)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    train_csv = os.path.join(output_directory, 'train.csv')
+    test_csv = os.path.join(output_directory, 'test.csv')
+    best_txt = os.path.join(output_directory, 'best.txt')
+
+    # create new csv files with only header
+    if not args.resume:
+        with open(train_csv, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+        with open(test_csv, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+    for epoch in range(start_epoch, args.epochs):
+        utils.adjust_learning_rate(optimizer, epoch, args.lr)
+        train(train_loader, model, criterion, optimizer, epoch) # train for one epoch
+        result, img_merge = validate(val_loader, model, epoch) # evaluate on validation set
+
+        # remember best rmse and save checkpoint
+        is_best = result.rmse < best_result.rmse
+        if is_best:
+            best_result = result
+            with open(best_txt, 'w') as txtfile:
+                txtfile.write("epoch={}\nmse={:.3f}\nrmse={:.3f}\nabsrel={:.3f}\nlg10={:.3f}\nmae={:.3f}\ndelta1={:.3f}\nt_gpu={:.4f}\n".
+                    format(epoch, result.mse, result.rmse, result.absrel, result.lg10, result.mae, result.delta1, result.gpu_time))
+            if img_merge is not None:
+                img_filename = output_directory + '/comparison_best.png'
+                utils.save_image(img_merge, img_filename)
+
+        utils.save_checkpoint({
+            'args': args,
+            'epoch': epoch,
+            'arch': args.arch,
+            'model': model,
+            'best_result': best_result,
+            'optimizer' : optimizer,
+        }, is_best, epoch, output_directory)
+
 
 
 def train(train_loader, model, criterion, optimizer, epoch):
@@ -123,5 +165,83 @@ def train(train_loader, model, criterion, optimizer, epoch):
             'gpu_time': avg.gpu_time, 'data_time': avg.data_time})
 
             
+def validate(val_loader, model, epoch, write_to_file=True):
+    average_meter = AverageMeter()
+    model.eval() # switch to evaluate mode
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        input, target = input.cuda(), target.cuda()
+        torch.cuda.synchronize()
+        data_time = time.time() - end
+
+        # compute output
+        end = time.time()
+        with torch.no_grad():
+            pred = model(input)
+        torch.cuda.synchronize()
+        gpu_time = time.time() - end
+
+        # measure accuracy and record loss
+        result = Result()
+        result.evaluate(pred.data, target.data)
+        average_meter.update(result, gpu_time, data_time, input.size(0))
+        end = time.time()
+
+        # save 8 images for visualization
+        skip = 50
+        if args.modality == 'd':
+            img_merge = None
+        else:
+            if args.modality == 'rgb':
+                rgb = input
+            elif args.modality == 'rgbd':
+                rgb = input[:,:3,:,:]
+                depth = input[:,3:,:,:]
+
+            if i == 0:
+                if args.modality == 'rgbd':
+                    img_merge = utils.merge_into_row_with_gt(rgb, depth, target, pred)
+                else:
+                    img_merge = utils.merge_into_row(rgb, target, pred)
+            elif (i < 8*skip) and (i % skip == 0):
+                if args.modality == 'rgbd':
+                    row = utils.merge_into_row_with_gt(rgb, depth, target, pred)
+                else:
+                    row = utils.merge_into_row(rgb, target, pred)
+                img_merge = utils.add_row(img_merge, row)
+            elif i == 8*skip:
+                filename = output_directory + '/comparison_' + str(epoch) + '.png'
+                utils.save_image(img_merge, filename)
+
+        if (i+1) % args.print_freq == 0:
+            print('Test: [{0}/{1}]\t'
+                  't_GPU={gpu_time:.3f}({average.gpu_time:.3f})\n\t'
+                  'RMSE={result.rmse:.2f}({average.rmse:.2f}) '
+                  'MAE={result.mae:.2f}({average.mae:.2f}) '
+                  'Delta1={result.delta1:.3f}({average.delta1:.3f}) '
+                  'REL={result.absrel:.3f}({average.absrel:.3f}) '
+                  'Lg10={result.lg10:.3f}({average.lg10:.3f}) '.format(
+                   i+1, len(val_loader), gpu_time=gpu_time, result=result, average=average_meter.average()))
+
+    avg = average_meter.average()
+
+    print('\n*\n'
+        'RMSE={average.rmse:.3f}\n'
+        'MAE={average.mae:.3f}\n'
+        'Delta1={average.delta1:.3f}\n'
+        'REL={average.absrel:.3f}\n'
+        'Lg10={average.lg10:.3f}\n'
+        't_GPU={time:.3f}\n'.format(
+        average=avg, time=avg.gpu_time))
+
+    if write_to_file:
+        with open(test_csv, 'a') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'mse': avg.mse, 'rmse': avg.rmse, 'absrel': avg.absrel, 'lg10': avg.lg10,
+                'mae': avg.mae, 'delta1': avg.delta1, 'delta2': avg.delta2, 'delta3': avg.delta3,
+                'data_time': avg.data_time, 'gpu_time': avg.gpu_time})
+    return avg, img_merge
+
+
 if __name__ == '__train__':
     main()
