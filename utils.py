25229f8a977e2cf3890b76b39a96948e10111311
alpharmike95 <55023198+alpharmike95@users.noreply.github.com>
2021-11-26 18:05:34 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -101,3 +101,15 @@ def adjust_learning_rate(optimizer, epoch, lr_init):
     lr = lr_init * (0.1 ** (epoch // 5))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
+
+        
+def save_checkpoint(state, is_best, epoch, output_directory):
+    checkpoint_filename = os.path.join(output_directory, 'checkpoint-' + str(epoch) + '.pth.tar')
+    torch.save(state, checkpoint_filename)
+    if is_best:
+        best_filename = os.path.join(output_directory, 'model_best.pth.tar')
+        shutil.copyfile(checkpoint_filename, best_filename)
+    if epoch > 0:
+        prev_checkpoint_filename = os.path.join(output_directory, 'checkpoint-' + str(epoch-1) + '.pth.tar')
+        if os.path.exists(prev_checkpoint_filename):
+            os.remove(prev_checkpoint_filename)
